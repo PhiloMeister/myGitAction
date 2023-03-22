a3510a1c5de85c0f2dc2851dba72d2678b2a8521
Abdullah <binjabdu@gmail.com>
2023-03-22 10:09:40 +0100

    Initial commit

---
+++ src/main/java/com/example/mygitaction/MyGitActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGitActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mygitaction/MyGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
